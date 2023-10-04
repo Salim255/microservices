f2375d28ecabab66ab984ff42d8c01e1ba06e03c
Salim <salim.hassan.welleat@gmail.com>
2023-10-04 15:51:45 +0200

    build: post index.js

---
+++ posts/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/post", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/post", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  //201 for created resource
+  res.status(201).send(posts[id]);
+});
+
+const posts = {};
+
+app.listen(4000, () => {
+  console.log("====================================");
+  console.log("Listening on 4000");
+  console.log("====================================");
+});
