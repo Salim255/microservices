adc682ee745ccb5141dde4b33c92e3fde86c2cdf
Salim <salim.hassan.welleat@gmail.com>
2023-10-31 10:59:48 +0100

    build: query and comment deployments

--- event-bus/index.js
+++ event-bus/index.js
@@ -18,10 +18,10 @@ app.post("/events", (req, res) => {
   axios.post("http://post-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://comment-clusterip-srv:4001/events", event).catch((err) => {
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://query-clusterip-srv:4002/events", event).catch((err) => {
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
     console.log(err.message, "here");
   });
 
